cc4ace59ffa98d94bee57f17fb7bff840658f046
mjdierkes <mjdierkes@msn.com>
2022-12-02 18:40:55 -0600

    Initial Commit

---
+++ Remember/RememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberApp.swift
+//  Remember
+//
+//  Created by Mason Dierkes on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
